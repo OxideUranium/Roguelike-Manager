bebbf2b824b2bac482d926cd10d9847eada8bf29
Ouyang <jingzhou.ouyang1@ucdconnect.ie>
2024-12-09 21:42:48 +0000

    AutoPack update

---
+++ autopack.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+
+
+file_name = "test.py"
+output_name = "test.exe"
+
+command = ["pyinstaller", "--onefile", file_name, "--name", output_name ]
+
+print(f"Running: {command}")
+
+result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+if result.returncode == 0:
+    print(f"\nBuild successful: 'dist/{output_name.split('.')[0]}'")
+else:
+    print("\nBuild failed:")
+    print(result.stderr)
